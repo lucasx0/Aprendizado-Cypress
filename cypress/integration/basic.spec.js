d3dd392b0b1d9981857efd492cc5139a91a6b060
Lucas Almeida <lucas.almeidasilva49@gmail.com>
2020-07-11 18:06:19 -0300

    Aprendendo debug e pause no cypress

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,19 +1,20 @@
 /// <reference types="cypress" />
 
 describe('cypress basics', () => {
-    it('Should visit a page and assert title', () =>{
+    it.only('Should visit a page and assert title', () =>{
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
 
         cy.title().should('be.equal', 'Campo de Treinamento')
-        cy.title().should('contain', 'Campo')
+        cy.title().should('contain', 'Campo').debug()
+        cy.pause()
 
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo')
-
+ 
     })
 
-    it.only('Should find and interact with an element', () => {
+    it('Should find and interact with an element', () => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
 
         cy.get('#buttonSimple').click()
