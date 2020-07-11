7b317a571e387468892d41770bbd13d162061d51
Lucas Almeida <lucas.almeidasilva49@gmail.com>
2020-07-11 19:00:16 -0300

    Links

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -8,4 +8,15 @@ describe('Work with basic elements', () => {
         cy.get('.facilAchar').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
+
+    it.only('Links', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.get('a').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+        cy.reload() // dar um refresh na tela.
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+    })
 })
