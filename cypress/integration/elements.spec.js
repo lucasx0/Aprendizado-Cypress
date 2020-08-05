d2a4d84ea713a49100188808dba0096011619649
lucas.almeida <lucas.almeida@usto.re>
2020-08-05 17:19:12 -0300

    aprendendo o uso do before e beforeeach

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,16 +1,24 @@
 /// <reference types="cypress" />
 
 describe('Work with basic elements', () => {
-    it('Text', () =>{
+    before(() =>{  //executa uma vez antes de todos os testes
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() =>{ //executa antes de cada um dos testes.
+        cy.reload()
+    })
+
+    it('Text', () =>{
+        
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
 
-    it.only('Links', () => {
-        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    it('Links', () => {
+        
         cy.get('a').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
 
